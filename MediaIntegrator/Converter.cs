d4e03e7e4a80a4785072b321129584ec351afa87
JimmyManninen <jimmy.manninen@bookbeat.com>
2020-04-27 18:41:44 +0200

    First version is done

---
+++ MediaIntegrator/Converter.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace MediaIntegrator
+{
+    public static class Converter
+    {
+        public static bool ConverterActive { get; set; }
+        public static string CsvPath { get; set; }
+        public static string XmlPath { get; set; }
+        
+        private enum CsvMusicProductTypes
+        {
+            Name,
+            Description,
+            Price,
+            Id,
+            Type,
+            Quantity,
+            Sales,
+            PlayTime,
+            Artist,
+            NotAvailable
+        }
+        
+        
+        /// <summary>
+        /// Start watching the location of the csv files for change
+        /// </summary>
+        public static void Start()
+        {
+            using (var watcher = new FileSystemWatcher())
+            {
+                watcher.Path = CsvPath;
+                watcher.NotifyFilter = NotifyFilters.LastWrite;
+                watcher.Changed += OnChanged;
+
+                watcher.EnableRaisingEvents = true;
+
+                while (ConverterActive)
+                {
+                }
+            }
+        }
+        
+        
+        /// <summary>
+        /// Method is called whenever a csv file have been modified
+        /// Only convert music products at the moment
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.Name == "Music.csv")
+                ConvertProductsCsvToXml(e.FullPath);
+        }
+        
+        
+        /// <summary>
+        /// Convert the products csv file to xml
+        /// Code inspired from http://getcodesnippet.com/2014/05/15/how-to-convert-csv-file-to-xml-file-in-c/
+        /// </summary>
+        /// <param name="csvFilePath"></param>
+        private static void ConvertProductsCsvToXml(string csvFilePath)
+        {
+            //var lines = File.ReadAllLines(csvFilePath).Skip(1);
+            var lines = new List<string>();
+            
+            ReadCsvLines(csvFilePath, lines);
+            
+            CreateXmlFromCsvLines(lines).Save(XmlPath + "\\" + "simplemedia.xml");
+        }
+
+        private static XElement CreateXmlFromCsvLines(IEnumerable<string> lines)
+        {
+            var xml = new XElement("Inventory",
+                from str in lines.Skip(1)
+                let columns = str.Split(',')
+                select new XElement("Item",
+                    new XElement("Name", columns[(int) CsvMusicProductTypes.Name]),
+                    new XElement("Count", columns[(int) CsvMusicProductTypes.Quantity]),
+                    new XElement("Price", columns[(int) CsvMusicProductTypes.Price]),
+                    new XElement("Comment", columns[(int) CsvMusicProductTypes.Description]),
+                    new XElement("Artist", columns[(int) CsvMusicProductTypes.Artist]),
+                    new XElement("Publisher", CsvMusicProductTypes.NotAvailable),
+                    new XElement("Genre", CsvMusicProductTypes.NotAvailable),
+                    new XElement("Year", "-1"),
+                    new XElement("ProductID", columns[(int) CsvMusicProductTypes.Id])
+                )
+            );
+            return xml;
+        }
+
+        private static void ReadCsvLines(string csvFilePath, ICollection<string> lines)
+        {
+            using (var stream = new FileStream(csvFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (var reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        lines.Add(line);
+                    }
+                }
+            }
+        }
+    }
+}
--- MediaIntegrator/MainWindow.xaml.cs
+++ MediaIntegrator/MainWindow.xaml.cs
@@ -1,25 +1,112 @@
-﻿using System.Windows;
+﻿using System;
+using System.Resources;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using Button = System.Windows.Controls.Button;
+using MessageBox = System.Windows.MessageBox;
 
 namespace MediaIntegrator
 {
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        private enum Buttons
+        {
+            StartConverterButton,
+            StopConverterButton,
+            CsvBrowseButton,
+            XmlBrowseButton
+        }
+
+        private enum ConverterStatus
+        {
+            Active,
+            Inactive
+        }
+        
         public MainWindow()
         {
             InitializeComponent();
+            
+            // Debug
+            Converter.CsvPath =
+                $"E:\\Projekt\\C#\\Distanskurs\\Laboration 3\\StoreSimulator\\StoreSimulator\\bin\\Debug\\Database";
+            Converter.XmlPath = $"C:\\Users\\manni\\Desktop";
+        }
+        
+        /// <summary>
+        /// Handles the browse button click for both xml and csv
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BrowseButtons_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is Button button)) return;
+            
+            var browserDialog = new FolderBrowserDialog();
+
+            if (browserDialog.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
+            
+            switch (button.Name)
+            {
+                case nameof(Buttons.CsvBrowseButton):
+                    Converter.CsvPath = browserDialog.SelectedPath;
+                    CsvPathText.Text = Converter.CsvPath;
+                    break;
+                case nameof(Buttons.XmlBrowseButton):
+                    Converter.XmlPath = browserDialog.SelectedPath;
+                    XmlPathText.Text = Converter.XmlPath;
+                    break;
+            }
+        }
+        
+        /// <summary>
+        /// Handles the start and stop functionality of the converter
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartStopConverterButtons_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!(sender is Button button)) return;
+
+            switch (button.Name)
+            {
+                case nameof(Buttons.StartConverterButton):
+                    if (!ValidateInput()) return;
+                    UpdateConverterStatus(ConverterStatus.Active);
+                    
+                    // Create a new thread because there is an infinite while loop in the Start method.
+                    Task.Run(Converter.Start);
+                    break;
+                
+                case nameof(Buttons.StopConverterButton):
+                    UpdateConverterStatus(ConverterStatus.Inactive);
+                    break;
+            }
         }
 
-        private void CsvBrowseButton_OnClick(object sender, RoutedEventArgs e)
+        private bool ValidateInput()
         {
-            throw new System.NotImplementedException();
+            if (PathsHaveBeenSelected()) return true;
+            
+            MessageBox.Show("You need to fill in the location of the csv and the save location for the xml file");
+            return false;
+
         }
 
-        private void XmlBrowseButton_OnClick(object sender, RoutedEventArgs e)
+        private bool PathsHaveBeenSelected()
         {
-            throw new System.NotImplementedException();
+            return Converter.CsvPath != null && Converter.XmlPath != null;
+        }
+        
+        private void UpdateConverterStatus(ConverterStatus state)
+        {
+            ConverterStatusText.Text = $"Status: {state}";
+            Converter.ConverterActive = state == ConverterStatus.Active;
         }
     }
 }
