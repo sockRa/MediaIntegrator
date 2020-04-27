362c1a6156ece3a556b572df89d20843d6aa72b2
JimmyManninen <jimmy.manninen@bookbeat.com>
2020-04-27 13:45:59 +0200

    Gui complete

---
+++ MediaIntegrator/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace MediaIntegrator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CsvBrowseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void XmlBrowseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
